c618e95b6a35060b030f60b0f3be1b47adf7b6d6
Airton Mendonça Silva <airtonmendoncasilva@gmail.com>
2020-12-01 18:25:43 -0300

    Criação das Comunidade.

--- ConnectingGamer/src/app/app.component.ts
+++ ConnectingGamer/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'Sobre',
       url: '/home',
       icon:'home' 
     },
--- ConnectingGamer/src/app/comunidade/comunidade.page.ts
+++ ConnectingGamer/src/app/comunidade/comunidade.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '../../../node_modules/@ionic/angular';
+import { AlertController, NavController } from '../../../node_modules/@ionic/angular';
 
 
 @Component({
@@ -9,11 +9,20 @@ import { NavController } from '../../../node_modules/@ionic/angular';
 })
 export class ComunidadePage implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private alertCtrl:AlertController) { }
 
   ngOnInit() {
   }
+  
+  async abrirAlertSimples(){
+    let alertSimples = await this.alertCtrl.create({
+      header : 'Olá Está com dúvida?',
+      message : 'Esta tela é sobre as comunidades que você pode participar no nosso aplicativo. Espero ter te ajudado :).',
+      buttons : ['OK']
+    });
 
+    await alertSimples.present();
+  }
   abrirPagina(nomeDaPagina : string){
     this.navCtrl.navigateForward(nomeDaPagina);
   }
