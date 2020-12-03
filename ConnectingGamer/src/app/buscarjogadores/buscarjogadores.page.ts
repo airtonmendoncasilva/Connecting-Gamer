b67b133d3d4758bc474f35aa2fa5f79ff53f9ef4
Airton Mendonça Silva <airtonmendoncasilva@gmail.com>
2020-12-03 20:29:30 -0300

    Alteração da tela de busca de jogadores.

--- ConnectingGamer/src/app/buscarjogadores/buscarjogadores.page.ts
+++ ConnectingGamer/src/app/buscarjogadores/buscarjogadores.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '../../../node_modules/@ionic/angular';
+import { LoadingController, NavController } from '../../../node_modules/@ionic/angular';
+
 
 @Component({
   selector: 'app-buscarjogadores',
@@ -7,15 +9,48 @@ import { NavController } from '../../../node_modules/@ionic/angular';
   styleUrls: ['./buscarjogadores.page.scss'],
 })
 export class BuscarjogadoresPage implements OnInit {
+users =[];
+page =0;
+maximumPages = 3;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private loadingCtrl : LoadingController, private http: HttpClient) {
+    this.loadUsers();
+   }
 
   ngOnInit() {
   }
+  async showLoadingDefault(){
+    let loading = await this.loadingCtrl.create({
+      message:'Buscando Jogadores...',
+      duration: 2000
+    });
 
-  abrirPagina(nomeDaPagina : string){
+    return await loading.present();
+  }
+  abrirPagina(nomeDaPagina: string) {
     this.navCtrl.navigateForward(nomeDaPagina);
   }
   
+  loadUsers(event?){
+    this.http.get('https://randomuser.me/api/?results=20&page=$(this.page)')
+    .subscribe(res =>{
+      console.log(res);
+      this.users = this.users.concat(res['results']);
 
+      if (event){
+        event.target.complete();
+      }
+    })
+  }
+
+  loadMore(event){
+    console.log(event);
+    this.page++;
+    this.loadUsers(event);
+
+    if(this.page === this.maximumPages){
+      event.target.disabled = true;
+    }
+  }
+ 
 }
